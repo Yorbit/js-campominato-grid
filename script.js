5f5fcd95bc0fea7baed2e501610fabf5269a37c6
Yorbit <marco.95112@hotmail.it>
2022-06-30 15:14:49 +0200

    add click and log event

--- script.js
+++ script.js
@@ -6,5 +6,23 @@ btnStart.addEventListener('click', function () {
     document.getElementById("btn-start-game").disabled = true;
 
     const parentGrid = document.getElementById("grid-game");
-    )
-}
+
+    parentGrid.classList.add('grid-container')
+
+    for (let index = 0; index < 100; index++) {
+
+        const newCell = createNewCell();
+
+        newCell.innerHTML = (index + 1);
+
+        parentGrid.append(newCell);
+
+        newCell.addEventListener('click', function () {
+
+            newCell.classList.toggle('active');
+
+            console.log('HAI CLICCATO IL NUMERO', + index + 1);
+        })
+    }
+})
+
