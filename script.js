3a911348c3ff1abc56b729241e85965f8c38c625
Yorbit <marco.95112@hotmail.it>
2022-06-30 14:57:24 +0200

    start js form btn

---
+++ script.js
@@ -0,0 +1,10 @@
+const btnStart = document.getElementById('btn-start-game')
+
+
+btnStart.addEventListener('click', function () {
+
+    document.getElementById("btn-start-game").disabled = true;
+
+    const parentGrid = document.getElementById("grid-game");
+    )
+}
